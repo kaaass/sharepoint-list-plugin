63481ba6dd34955b61964354a1354186f7538eeb
KAAAsS <admin@kaaass.net>
2021-09-17 15:46:04 +0800

    关闭最小化代码输出以符合 GreasyFork 相关规则

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,5 +7,9 @@ module.exports = merge(common(), {
     // use @require in header to import vue
     vue: 'Vue',
     'element-ui': 'element-ui'
-  }
+  },
+  // 根据 Greasy Fork 规则取消最小化
+  optimization: {
+    minimize: false
+  },
 })
