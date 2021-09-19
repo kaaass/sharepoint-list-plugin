b8403cb71634688d73f355557ccc0fcd207b9fea
KAAAsS <admin@kaaass.net>
2021-09-19 12:37:37 +0800

    禁止 iframe 内运行脚本

--- tampermonkey.js
+++ tampermonkey.js
@@ -14,4 +14,5 @@
 // @require      https://cdn.bootcdn.net/ajax/libs/vue/2.6.12/vue.min.js
 // @require      https://unpkg.com/element-ui@2.14.1/lib/index.js
 // @connect      *
+// @noframes
 // ==/UserScript==
